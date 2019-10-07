957ba2446a64786b33ba85e6fab827b7c1ab8b39
Renee Lujo <info@reneelujo.me>
2019-10-07 10:01:10 -0400

    another commit

---
+++ Hello/src/BeginingLearningJava.java
@@ -0,0 +1,17 @@
+
+public class BeginingLearningJava {
+
+	public static void main(String[] args) { //methods-they usually do something
+		System.out.println("Hello There"); //statement is telling computer to do something
+		
+	}
+
+	//String x = new String("hello");  //property-stores value 
+	
+	//class contains everything and has members
+	//access modifier (public) - who can use  the class
+	//static - no instance of a class needed 
+	//object- instance of a class
+	//arguments are what you pass to a method (part of the calling)
+	//parameter are variables to store arguments (part of the definition)
+}
---
+++ Hello/src/practice.java
@@ -0,0 +1,17 @@
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int i = 
+		for (i=3, i<=4, i==1);
+			System.out.println(i + "You got it");
+			
+			}
+		
+
+	}
+
+
