ad135dafe712b65242d47bc21cfac10b15ef5dea
Renee Lujo <info@reneelujo.me>
2019-10-07 09:59:17 -0400

    second practice commit

---
+++ firstproject/src/com/icici/loans/carloans/FirstClass.java
@@ -0,0 +1,15 @@
+package com.icici.loans.carloans;
+
+import java.io.FileInputStream;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
+		
+
+	}
+
+}
